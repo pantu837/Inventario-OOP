59f61536ebee333dd106366f372da688eca525d3
axel <pantu837@gmail.com>
2026-07-15 11:54:26 -0600

    feat: crear ProductoDAO con operaciones CRUD

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 from src.db import crear_tabla
 from src.modelo import Producto
+from src.dao import ProductoDAO
 
 
 def main():
 
     crear_tabla()
 
+    dao = ProductoDAO()
+
+
     producto = Producto(
-        1,
+        None,
         "Mouse",
         250,
         5
     )
 
-    print(producto.nombre)
-    print(producto.precio)
-    print(producto.stock)
+
+    dao.guardar(producto)
+
+
+    resultado = dao.buscar_por_id(1)
+
+
+    print(resultado.nombre)
+    print(resultado.precio)
+    print(resultado.stock)
+
 
 
 if __name__ == "__main__":
